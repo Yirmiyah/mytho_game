1ebc24eb595f0d298039be506ddc19a0df738e6f
Student07 <santoslukombo@gmail.com>
2023-02-23 18:05:33 +0100

    ready to merge

--- nui/assets/js/game.js
+++ nui/assets/js/game.js
@@ -4,8 +4,8 @@ const timerDisplay = document.getElementById("timer");
 
 let count = 60;
 
-startButton.addEventListener("click", function() {
-  const countdown = setInterval(function() {
+startButton.addEventListener("click", function () {
+  const countdown = setInterval(function () {
     count--;
     timerDisplay.innerHTML = count < 10 ? "0" + count.toString() : count;
 
@@ -31,9 +31,6 @@ var card = document.querySelector('.card');
 /* End of flip card*/
 
 
-
-
-
 /* Shuffle card*/
 let manche1 = false;
 
@@ -51,16 +48,54 @@ shuffle.addEventListener("click", () => {
   cardList.classList.add("is-animated");
   shuffle.disabled = true;
   reset.disabled = false;
-  setTimeout(function() {
+  setTimeout(function () {
     card.classList.toggle('is-flipped');
   }, 4000);
-  });
+});
 
 reset.addEventListener("click", () => {
   cardList.classList.remove("is-animated");
   card2List.classList.add("is-animated");
-      
+
   reset.disabled = true;
   shuffle.disabled = false;
   manche1 = true;
+});
+
+/*Randomiser les Questions/Réponses */
+
+
+function PickQRL(){
+
+  fetch('quizz.json')
+  .then(response => response.json())
+  .then(data => {
+    // console.log(data);
+    let Questions = data.Question
+    let Reponses = data.Response
+    let Level = data.Level
+
+    numberRand = Math.floor(Math.random() * 93);
+    Questions[numberRand]
+    Reponses[numberRand]
+    Level[numberRand]
+
+    console.log("Questions: " + Questions[numberRand])
+    console.log("Responses: " + Reponses[numberRand])
+    console.log("Level: " + Level[numberRand])
+
+    const Q = `${Questions}`;
+    const R = `${Reponses}`;
+    document.getElementById("Question").innerHTML = Q;
+    document.getElementById("Reponse").innerHTML = R;
+
+  })
+  .catch(error => {
+    console.error('Une erreur est survenue', error);
   });
+
+}
+
+
+
+
