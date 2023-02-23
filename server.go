1bdccae1ec556abcd81d81376cd4e9f2fdb22fbf
max92 <lmaxla90002@gmail.com>
2023-02-23 11:50:48 +0100

    first commit

--- server.go
+++ server.go
@@ -34,14 +34,8 @@ func main() {
 
 	http.Handle("/", fileServer)
 	http.HandleFunc("/error", erreur)
-	http.HandleFunc("/game", Game)
-
 	fmt.Printf("Starting server at port:8080\n")
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal(err)
 	}
-}
-
-func Game(w http.ResponseWriter, r *http.Request) {
-
 }
